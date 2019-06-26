15677fad1ba1fd08cd29222b0a492fabcd2737a1
wang_ghjm@163.com <wang_ghjm@163.com>
2019-06-26 16:51:31 +0800

    ...

---
+++ springboot-zookeeper-client/src/main/java/com/example/zk/client/SpringbootZookeeperClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.zk.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZookeeperClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootZookeeperClientApplication.class, args);
+    }
+
+}
